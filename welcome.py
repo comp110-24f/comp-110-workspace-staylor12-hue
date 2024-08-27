d2f791c5a763d79495aa8849db28d3d7caa6b37f
Sophie Taylor <sopannet@unc.edu>
2024-08-27 15:35:57 -0400

    Finished Exercise 00!

--- welcome.py
+++ welcome.py
@@ -5,3 +5,9 @@ __author__ = "01234567"
 print("Welcome to COMP110!")
 print("You are in for a fun adventure into programming!")
 print("<3 the COMP110 Team!")
+from random import random
+
+random()
+from random import choice
+
+choice()
